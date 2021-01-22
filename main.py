9701bf048256c158874e0377e3a756bafd6df2dc
Iain Muir <iam9ez@virginia.edu>
2021-01-22 17:28:41 -0500

    Main/Home Organization / File deletion

--- main.py
+++ main.py
@@ -3,26 +3,33 @@ Iain Muir, iam9ez
 
 PROJECT DESCRIPTION
 
+# TODO
+    • Make Newspage more robust (single data source)
+    • Overhead ticker (ex. WSJ)
+    • Expand / Collapse (containers?)
+    •
+
 streamlit run /Users/iainmuir/PycharmProjects/Desktop/streamlitApp/main.py
 """
 
 import streamlit as st
 
-# from espnFantasyFootball import app as fantasy_app
-from sportsHighlights import app as sh_app
-from stockMarket import app as sm_app
+from espnFantasyFootball import fantasy_app
+from sportsHighlights import sports_app
+from stockMarket import market_app
 
-import home
+import home_page
 
 # st.set_page_config(layout="wide")
 
 PAGES = {
-    "Home": home,
-    "Marketplace Info": sm_app,
-    "Sports Center": sh_app
+    "Home": home_page,
+    "Marketplace Info": market_app,
+    "Sports Center": sports_app,
+    "Fantasy": fantasy_app
 }
 
-st.sidebar.title("Navigation")
+st.sidebar.title("Main Navigation")
 selection = st.sidebar.radio("Go to", list(PAGES.keys()))
 page = PAGES[selection]
 page.launch()
